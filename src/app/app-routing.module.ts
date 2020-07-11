5eb579a3e24a448191245273e502c8b79f15c569
Justin Li <dzjustinli2@gmail.com>
2020-07-11 23:05:10 +1000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'heroes',
+        loadChildren: () => import('./hero/hero.module').then(m => m.HeroModule)
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// self defined components
+import { AboutComponent } from './about/about.component';
+import { BannerComponent } from './banner/banner.component';
+import { TwainComponent } from './twain/twain.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+// self defined services
+import { UserService } from './model/user.service';
+import { HeroService } from './model/hero.service';
+import { TwainService } from './twain/twain.service';
+
+// self defined modules
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+
+// in memory database service
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        
+        // self defined component
+        BannerComponent,
+        AboutComponent,
+        TwainComponent,
+        WelcomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DashboardModule,
+        SharedModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService,
+            {
+                dataEncapsulation: false
+            }
+        )
+    ],
+    providers: [
+        // self defined services
+        UserService,
+        HeroService,
+        TwainService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
