bbb8fbac1cd089b2b777b3a490bef185f7e56065
Justin Li <dzjustinli2@gmail.com>
2020-07-11 23:20:23 +1000

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: '<h1>{{title}}</h1>'
 })
-export class AppComponent {}
+export class AppComponent {
+    title = 'testing-playground';
+}
+
+//import { Component } from '@angular/core';
+
+//@Component({
+//    selector: 'app-root',
+//    templateUrl: './app.component.html',
+//    styleUrls: ['./app.component.css']
+//})
+//export class AppComponent { }
